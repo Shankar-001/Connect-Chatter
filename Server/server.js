32be01630347ca88814f224b2e8edb8a814e5631
Shankar-001 <praveen01.ugec20@iiitranchi.ac.in>
2023-08-04 02:48:38 +0530

    Deploy

--- Server/server.js
+++ Server/server.js
@@ -27,7 +27,7 @@ const server = app.listen(PORT, console.log(`Server is running on ${PORT}`));
 const io = require('socket.io')(server, {
   pingTimeout: 60000,
   cors: {
-    origin: 'http://localhost:3000',
+    origin: '*',
   },
 });
 
